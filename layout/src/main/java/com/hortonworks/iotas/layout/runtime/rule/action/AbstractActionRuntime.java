d4f88197eb8c60d0adc223ad231e2588348e4fc2
Satish Duggana <sduggana@hortonworks.com>
2016-04-13 15:55:21 +0530

    IOT-188 Added split/join/stage implementation for runtime

--- layout/src/main/java/com/hortonworks/iotas/layout/runtime/rule/action/AbstractActionRuntime.java
+++ layout/src/main/java/com/hortonworks/iotas/layout/runtime/rule/action/AbstractActionRuntime.java
@@ -18,10 +18,17 @@
  */
 package com.hortonworks.iotas.layout.runtime.rule.action;
 
+import com.hortonworks.iotas.client.CatalogRestClient;
+import com.hortonworks.iotas.util.CoreUtils;
 import com.hortonworks.iotas.util.ProxyUtil;
+import org.apache.commons.io.IOUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Map;
 
 /**
@@ -32,7 +39,8 @@ public abstract class AbstractActionRuntime implements ActionRuntime {
 
     protected ActionRuntimeContext actionRuntimeContext;
     protected Map<String, Object> config;
-
+    protected CatalogRestClient catalogRestClient;
+    protected String localJarPath;
 
     @Override
     public void setActionRuntimeContext(ActionRuntimeContext actionRuntimeContext) {
@@ -42,11 +50,37 @@ public abstract class AbstractActionRuntime implements ActionRuntime {
     @Override
     public void initialize(Map<String, Object> config) {
         this.config = config;
+        if(!config.containsKey(CoreUtils.CATALOG_ROOT_URL)) {
+            log.warn("[{}] is not configured in topology", CoreUtils.CATALOG_ROOT_URL);
+            return;
+        }
+        if(!config.containsKey(CoreUtils.LOCAL_JAR_PATH)) {
+            log.warn("[{}] is not configured in topology", CoreUtils.LOCAL_JAR_PATH);
+            return;
+        }
+
+        String catalogRootURL = config.get(CoreUtils.CATALOG_ROOT_URL).toString();
+        localJarPath = config.get(CoreUtils.LOCAL_JAR_PATH).toString();
+        catalogRestClient = new CatalogRestClient(catalogRootURL);
+        log.info("Local jar storage path:[{}], CatalogRestClient: [{}]", localJarPath, catalogRestClient);
     }
 
     protected String getJarPathFor(Long jarId) {
-        // IOT-226 takes care of this.
-        return null;
+        if(catalogRestClient == null) {
+            throw new IllegalStateException("catalogRestClient is not yet initialized.");
+        }
+        final InputStream inputStream = catalogRestClient.getJar(jarId);
+        final String jarPath = String.format("%s%sjar-%s.jar", localJarPath, File.separator, jarId);
+        log.info("Creating local jar file [{}]", jarPath);
+
+        try {
+            IOUtils.copy(inputStream, new FileOutputStream(new File(jarPath)));
+        } catch (IOException e) {
+            log.error("Encountered error while writing file [{}]", jarPath, e);
+            throw new RuntimeException(e);
+        }
+
+        return jarPath;
     }
 
     /**
